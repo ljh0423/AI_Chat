94a101df17847895801c14e68ae3262adc9bec97
Juhyun Lee <78312165+ljh0423@users.noreply.github.com>
2025-06-10 17:53:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://app.onecompiler.com"],
+    allow_origins=["https://ljh0423.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
