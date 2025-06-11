43961936319f96aa825756832c2a6c81dd3d70a2
Juhyun Lee <78312165+ljh0423@users.noreply.github.com>
2025-06-10 21:50:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ljh0423.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
